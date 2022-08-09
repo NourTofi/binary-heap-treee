5d540e24f4d74e4bd4bb09992149728a4bb9e512
Nour Tofi <69059723+NourTofi@users.noreply.github.com>
2022-08-09 22:34:59 +0300

    left has been done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import { BinarySearchTreeNode, drawBinaryTree } from
 'binary-tree-visualizer';
 
+import lft from './leftNode';
+
 const root = new BinarySearchTreeNode(100);
+
+root.insert(lft);
+
 drawBinaryTree(root, document.querySelector('canvas'));
---
+++ src/leftNode.js
@@ -0,0 +1,9 @@
+import { BinarySearchTreeNode } from
+'binary-tree-visualizer';
+
+function lft() {
+  const left = new BinarySearchTreeNode(50);
+  return (left);
+}
+
+export default lft;
