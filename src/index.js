c50b85bf52217798525815876ae459ee3a40d2b5
Mohamed Raslan <mohammed310.mr@gmail.com>
2022-08-21 14:45:24 +0300

    First try to draw.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-// import { BinarySearchTreeNode, drawBinaryTree } from
-//     'binary-tree-visualizer';
+import { BinaryTreeNode, drawBinaryTree } from
+    'binary-tree-visualizer';
 
 // The tree.
-let nodes = [null, 10, 14, 12, 9, 5, 4,4];
+let nodes = [null, 10, 14, 4,6,5,7];
+
 
 // To make the node is the root.
 function swap(node, root) {
@@ -13,11 +14,27 @@ function swap(node, root) {
 
 function double() {
     for (let node = 0; node < nodes.length; node++) {
-        if (nodes[node-1] === nodes[nodes.length -1]) {
+        if (nodes[node - 1] === nodes[nodes.length - 1]) {
             nodes.pop();
         }
     }
 }
+
+
+
+function drawTree() {
+    sort();
+    let root = new BinaryTreeNode(nodes[1]);
+    for (let node = 2; node < nodes.length; node++) {
+        if (node % 2 === 0)
+            root.setLeft(new BinaryTreeNode(nodes[node]));
+
+        else if (node % 2 !== 0)
+            root.setRight(new BinaryTreeNode(nodes[node]));
+        drawBinaryTree(root, document.querySelector('canvas'));
+    }
+}
+drawTree();
 
 // Sorting the list as binary heap tree.
 function sort() {
@@ -58,14 +75,14 @@ function sort() {
     }
 }
 
-sort();
+
 for (let i = 1; i < nodes.length; i++) {
 
     console.log(nodes[i]);
 }
 
 
-// const root = new BinarySearchTreeNode(100);
 
 
-// drawBinaryTree(root, document.querySelector('canvas'));
+
+
